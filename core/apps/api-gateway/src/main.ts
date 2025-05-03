6fcc40cf4c78c9bab8a277680a5ee7c2487a2d7c
lakshyarawat1 <lakshya.paramount@gmail.com>
2025-05-03 11:12:58 +0530

    [ #2 Addition - Added a rate limiter to api-gateway ]

--- core/apps/api-gateway/src/main.ts
+++ core/apps/api-gateway/src/main.ts
@@ -4,17 +4,48 @@
  */
 
 import express from 'express';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import swaggerUi from 'swagger-ui-express';
 import * as path from 'path';
+import axios from 'axios';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  allowedHeaders: ["Authorization", "Content-Type"],
+  credentials: true,
+}),
+);
+
+app.use(morgan('dev'));
+app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+app.use(cookieParser());
+app.set("trust proxy", 1);
+
+// Rate limiting
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: (req: any) => (req.user ? 1000 : 100), // limit each IP to 1000 requests per windowMs
+  message: { error: "Too many requests, please try again later." },
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: true,
+  keyGenerator: (req: any) => req.ip,
+})
+
+app.use(limiter);
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
